61779aae94cd0cb6bc4392a3288a45171d6b6fc4
jaggu <jaggu@DESKTOP-OSOHI7F>
2026-06-27 17:16:36 -0600

    V1: Initial commit: Hello World Print

---
+++ Interview_Prep/src/pageclass/HelloWorld.java
@@ -0,0 +1,11 @@
+package pageclass;
+
+public class HelloWorld {
+
+	public void printHelloWorld() {
+		System.out.println("Testing First Line of Code: HELLO WORLD");
+	}
+	
+
+
+}
---
+++ Interview_Prep/src/testcases/HelloWorldTestcase.java
@@ -0,0 +1,12 @@
+package testcases;
+
+import pageclass.HelloWorld;
+
+public class HelloWorldTestcase {
+
+	public static void main(String[] args) {
+		HelloWorld helloWorld= new HelloWorld();
+		helloWorld.printHelloWorld();
+	}
+
+}
