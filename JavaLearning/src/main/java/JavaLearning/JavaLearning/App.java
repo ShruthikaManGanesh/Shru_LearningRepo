c73b00aae643f5a987f89f443b01b5dad72b29dd
jaggu <jaggu@DESKTOP-OSOHI7F>
2026-06-28 11:10:50 -0600

    Initial commit: V1

---
+++ JavaLearning/src/main/java/JavaLearning/JavaLearning/App.java
@@ -0,0 +1,10 @@
+package JavaLearning.JavaLearning;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
